2b2821f42ef078b5ec203da4ec322f24c19f26f8
Jacob Levine <Jacob.levine93@protonmail.com>
2018-09-17 14:09:15 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+
+  var config = {
+    apiKey: "AIzaSyBSSLizzuR528vMP4u3PB04pXJrDdc_dno",
+    authDomain: "bloc-chat-90637.firebaseapp.com",
+    databaseURL: "https://bloc-chat-90637.firebaseio.com",
+    projectId: "bloc-chat-90637",
+    storageBucket: "bloc-chat-90637.appspot.com",
+    messagingSenderId: "141815988728"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
